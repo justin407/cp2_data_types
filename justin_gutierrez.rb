1db3d119de83d1fa3c619d5846de4f6fc437508d
justin407 <jgutierrez@chicagotechacademy.org>
2014-10-03 12:55:49 -0500

    Create justin_gutierrez.rb

---
+++ justin_gutierrez.rb
@@ -0,0 +1,11 @@
+justin = { :age => 17, :height => "5'6\"" }
+
+def say_justin
+  "justin"
+end
+
+space = " "
+
+puts say_justin + " is" + space + justin[:age].to_s + "."
+
+puts say_justin y + " is" + space + justin[:height] + "."
